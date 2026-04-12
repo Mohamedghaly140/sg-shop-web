92c27f5a019bba2560312c8bc4e312bbf7e2eaad
mohamedghaly140 <mohamedghaly140@gmail.com>
2026-04-12 16:09:16 +0200

    feat: wire storefront layout with data-theme scope, nav, and footer

--- app/(storefront)/layout.tsx
+++ app/(storefront)/layout.tsx
@@ -1,13 +1,16 @@
+import { StorefrontNav } from "./_components/nav";
+import { StorefrontFooter } from "./_components/footer";
+
 export default function StorefrontLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <header>Storefront Nav</header>
-      <main className="flex-1">{children}</main>
-      <footer>Storefront Footer</footer>
-    </>
+    <div data-theme="storefront" className="min-h-screen flex flex-col">
+      <StorefrontNav />
+      <main className="flex-1 pt-20">{children}</main>
+      <StorefrontFooter />
+    </div>
   );
 }
